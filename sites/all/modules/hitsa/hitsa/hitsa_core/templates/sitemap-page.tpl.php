7ad1f34a5bfe85fad9bc7a8285853b9736ceb460
Madis Loik <madis@wiseman.ee>
2017-11-01 10:14:18 +0000

    Sassi muudatused

---
+++ sites/all/modules/hitsa/hitsa/hitsa_core/templates/sitemap-page.tpl.php
@@ -0,0 +1,112 @@
+<div class="block">
+
+  <h1 class="block-title"><?php print drupal_get_title(); ?></h1>
+  
+  <div class="row">
+    <div class="col-4 sm-12">
+      <div class="sitemap">
+                        <h3 class="heading-link"><a href="">AVALEHT</a></h3>
+                        <h3>SISSEASTUMINE</h3>
+                        <ul>
+                           <li><a href="">Erialad</a></li>
+                           <li><a href="">Vastuvõtt</a></li>
+                           <li><a href="">Töökohapõhine õpe</a></li>
+                           <li><a href="">Ettevalmistuskursused</a></li>
+                           <li><a href="">Avatud uste päev</a></li>
+                           <li><a href="">Tudengi vari</a></li>
+                           <li><a href="">Mittestatsionaarne õpe</a></li>
+                           <li><a href="">Kaugõpe</a></li>
+                        </ul>
+                        <h3>ÕPPETÖÖ</h3>
+                        <ul>
+                           <li><a href="">Õppetöö kalender</a></li>
+                           <li><a href="">Tunniplaan</a></li>
+                           <li><a href="">VÕTA</a></li>
+                           <li><a href="">Lõpetamine</a></li>
+                           <li><a href="">Õpilastööde vormistamine</a></li>
+                           <li><a href="">Õpiränne</a></li>
+                           <li><a href="">Praktika</a></li>
+                           <li><a href="">Vabaained</a></li>
+                           <li><a href="">Valikained</a></li>
+                        </ul>
+                        <h3>KOOLIELU</h3>
+                        <ul>
+                           <li><a href="">Kodukord</a></li>
+                           <li><a href="">Majutus</a></li>
+                           <li><a href="">Toitlustus</a></li>
+                           <li><a href="">Raamatukogu</a></li>
+                           <li><a href="">Huvitegevuse kalender</a></li>
+                           <li><a href="">Huvitegevused</a></li>
+                           <li><a href="">Sportimisvõimalused</a></li>
+                           <li><a href="">Nõustamine</a></li>
+                           <li><a href="">Toetused</a></li>
+                        </ul>
+                     </div><!--/sitemap-->
+    </div><!--/col-4-->
+    <div class="col-4 sm-12">
+                     <div class="sitemap">
+                        <h3>Täiendusõpe</h3>
+                        <ul>
+                           <li><a href="">Täiendusõppe toetused</a></li>
+                           <li><a href="">E-kursused</a></li>
+                           <li><a href="">Koolitused</a></li>
+                           <li><a href="">Kutseeksamid</a></li>
+                           <li><a href="">Tellitavad koolitused</a></li>
+                        </ul>
+                        <h3>Teenused</h3>
+                        <ul>
+                           <li><a href="">Avalikud</a>
+                              <ul class="bullet-list_article">
+                                 <li><a href="">Teenus 1</a></li>
+                                 <li><a href="">Teenus 2</a></li>
+                                 <li><a href="">Teenus 3</a></li>
+                                 <li><a href="">Teenus 4</a></li>
+                                 <li><a href="">Teenus 5</a></li>
+                              </ul><!--/bullet-list_article-->
+                           </li>
+                           <li><a href="">Koolisisesed</a>
+                              <ul class="bullet-list_article">
+                                 <li><a href="">Teenus 1</a></li>
+                                 <li><a href="">Teenus 2</a></li>
+                                 <li><a href="">Teenus 3</a></li>
+                                 <li><a href="">Teenus 4</a></li>
+                                 <li><a href="">Teenus 5</a></li>
+                              </ul><!--/bullet-list_article-->
+                           </li>
+                           <li><a href="">Ettevõtjale</a>
+                              <ul class="bullet-list_article">
+                                 <li><a href="">Teenus 1</a></li>
+                                 <li><a href="">Teenus 2</a></li>
+                                 <li><a href="">Teenus 3</a></li>
+                              </ul><!--/bullet-list_article-->
+                           </li>
+                        </ul>
+                     </div><!--/sitemap-->
+                  </div><!--/col-4-->
+    <div class="col-4 sm-12">
+                     <div class="sitemap">
+                        <h3>Meie koolist</h3>
+                        <ul>
+                           <li><a href="">Kooli väljaanded</a></li>
+                           <li><a href="">Virtuaaltuur</a></li>
+                           <li><a href="">Põhiväärtused</a></li>
+                           <li><a href="">Traditsioonid</a></li>
+                           <li><a href="">Kooli sümboolika</a></li>
+                           <li><a href="">Galerii</a></li>
+                           <li><a href="">Tunnustused</a></li>
+                           <li><a href="">Õpilasesindus</a></li>
+                           <li><a href="">Kooli nõukogu</a></li>
+                           <li><a href="">Nõunike kogu</a></li>
+                           <li><a href="">Koostöö ja projektid</a></li>
+                           <li><a href="">Vilistlased</a></li>
+                           <li><a href="">Tööpakkumised</a></li>
+                           <li><a href="">Arengukava</a></li>
+                           <li><a href="">Põhimäärus</a></li>
+                           <li><a href="">Eeskirjad</a></li>
+                        </ul>
+                        <h3 class="heading-link"><a href="">Kontakt</a></h3>
+                     </div><!--/sitemap-->
+                  </div><!--/col-4-->
+  </div><!--/row-->
+  
+</div><!--/block-->
