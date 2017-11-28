69e2fa2c28f4f4156298f80d4288edcd25afb12a
Madis Loik <madis@wiseman.ee>
2017-11-28 09:22:29 +0000

    Contact page changes. Switching to block logic.

---
+++ sites/all/modules/hitsa/hitsa/hitsa_contacts/modules/hitsa_locations/templates/hitsa-locations-block.tpl.php
@@ -0,0 +1,89 @@
+<?php if(!empty($locations)): ?>
+  <?php foreach($locations as $location): ?>
+  <div class="block block-narrow">
+    <h2 class="block-title"><?php print check_plain($location->title); ?></h2>
+    
+    <div class="accordion accordion-xs" data-plugin="accordion">
+                  
+      <div class="accordion-entry">
+        <div class="accordion-title"><?php print t('General information'); ?></div>
+        <div class="accordion-content">
+                      
+          <ul class="list-details">
+            <?php if(!empty($location->location_address)): ?>
+            <li>
+              <div class="before-home"></div>
+              <div class="list-details_text">
+                <p><?php print check_plain($location->location_address[LANGUAGE_NONE][0]['safe_value']); ?> </p>
+              </div><!--/list-details_text-->
+            </li>
+            <?php endif; ?>
+            <?php if(!empty($location->phone_nr)): ?>
+            <li>
+            <div class="before-phone"></div>
+            <div class="list-details_text">
+              <p><?php print check_plain($location->phone_nr[LANGUAGE_NONE][0]['safe_value']); ?></p>
+            </div><!--/list-details_text-->
+            </li>
+            <?php endif; ?>
+            <?php if(!empty($location->e_mail)): ?>
+            <li>
+              <div class="before-envelope"></div>
+              <div class="list-details_text">
+                <p>
+                  <a href="mailto:<?php print check_plain($location->e_mail[LANGUAGE_NONE][0]['email']); ?>"><?php print check_plain($location->e_mail[LANGUAGE_NONE][0]['email']); ?></a>
+                </p>
+              </div><!--/list-details_text-->
+            </li>
+            <?php endif; ?>
+            <?php if(!empty($location->location_visiting_hours)): ?>
+            <li>
+              <div class="before-unlock"></div>
+              <div class="list-details_text">
+                <p><?php print check_plain($location->location_visiting_hours[LANGUAGE_NONE][0]['safe_value']); ?></p>
+              </div><!--/list-details_text-->
+            </li>
+            <?php endif; ?>
+          </ul><!--/list-details-->
+                      
+        </div><!--/accordion-content-->
+      </div><!--/accordion-entry-->
+      
+      <?php if(!empty($location->location_coordinates)): ?>
+      <div class="accordion-entry">
+        <div class="accordion-title"><?php print t('Map'); ?></div>
+        <div class="accordion-content">
+          <div class="map-wrapper">
+						<div class="map" data-plugin="googleMaps" data-coords="<?php 
+						print sprintf('%s,%s', $location->location_coordinates[LANGUAGE_NONE][0]['lat'], 
+						$location->location_coordinates[LANGUAGE_NONE][0]['lng']); ?>" 
+						data-icon="<?php print '/' . drupal_get_path('theme', $GLOBALS['theme']) . '/static/assets/imgs/mapIcon.png'; ?>" data-zoom="15"></div>
+						<img src="<?php print '/' . drupal_get_path('theme', $GLOBALS['theme']) . '/static/assets/imgs/placeholder-56.gif'; ?>">
+					</div>
+        </div><!--/accordion-content-->
+      </div><!--/accordion-entry-->
+      <?php endif; ?>
+      
+      <?php if(!empty($location->location_parking)): ?>
+      <div class="accordion-entry">
+        <div class="accordion-title"><?php print t('Parking'); ?></div>
+        <div class="accordion-content">
+           <?php print nl2br(check_plain($location->location_parking[LANGUAGE_NONE][0]['safe_value'])); ?>
+        </div><!--/accordion-content-->
+      </div><!--/accordion-entry-->
+      <?php endif; ?>
+      
+      <?php if(!empty($location->location_transport)): ?>
+      <div class="accordion-entry">
+        <div class="accordion-title"><?php print t('Transportation'); ?></div>
+        <div class="accordion-content">
+          <?php print nl2br(check_plain($location->location_transport[LANGUAGE_NONE][0]['safe_value'])); ?>
+        </div><!--/accordion-content-->
+      </div><!--/accordion-entry-->
+      <?php endif; ?>
+        
+    </div><!--/accordion-->
+               
+  </div><!--/block-->
+  <?php endforeach; ?>
+<?php endif; ?>
