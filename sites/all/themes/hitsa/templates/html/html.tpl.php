3e7cb697b34b3afa1f2452013f70828e8705f05b
Madis Loik <madis@wiseman.ee>
2017-12-19 11:13:38 +0000

    Validation additions.

--- sites/all/themes/hitsa/templates/html/html.tpl.php
+++ sites/all/themes/hitsa/templates/html/html.tpl.php
@@ -45,7 +45,7 @@
 ?>
 
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
+<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
 
 <head profile="<?php print $grddl_profile; ?>">
 
