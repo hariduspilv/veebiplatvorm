80c781e71409dd695839488ca7547e962a7c90c3
Madis Loik <madis@wiseman.ee>
2018-01-03 12:37:58 +0000

    Page spacing fix. Autocomplete filter additions. Bugfixes

--- sites/all/modules/hitsa/hitsa/hitsa_content_page/templates/hitsa-content-output.tpl.php
+++ sites/all/modules/hitsa/hitsa/hitsa_content_page/templates/hitsa-content-output.tpl.php
@@ -1,14 +1,14 @@
 <?php foreach($nodes as $tid => $academic_year): ?>
   <div class="row">
-        <div class="col-12">
+    <div class="col-12">
+      
+      <?php if(!empty($academic_year_keyed[$tid])): ?>
+      <h3><?php print check_plain($academic_year_keyed[$tid]); ?></h3>
+      <?php endif; ?>
+    </div><!--/col-12-->
+  </div><!--/row-->
           
-          <?php if(!empty($academic_year_keyed[$tid])): ?>
-          <h3><?php print check_plain($academic_year_keyed[$tid]); ?></h3>
-          <?php endif; ?>
-        </div><!--/col-12-->
-      </div><!--/row-->
-          
-  <div class="row-spacer-xs"></div>
+  <div class="row-spacer-xs sm-hide"></div>
   
   <div class="row row-vertical-xl">
     <?php foreach($academic_year as $academic_year_nodes): ?>
@@ -17,10 +17,10 @@
   </div><!--/row-->
   
   <?php if(next($nodes) !== FALSE): ?>
-  <div class="row-spacer"></div>
-  <div class="row-spacer"></div>
+  <div class="row-spacer sm-hide"></div>
+  <div class="row-spacer sm-hide"></div>
   <hr>
-  <div class="row-spacer"></div>
+  <div class="row-spacer sm-hide"></div>
   <?php endif; ?>
 
 <?php endforeach; ?>
--- sites/all/modules/hitsa/hitsa/hitsa_content_page/templates/hitsa-content-page.tpl.php
+++ sites/all/modules/hitsa/hitsa/hitsa_content_page/templates/hitsa-content-page.tpl.php
@@ -1,8 +1,5 @@
-<div class="row">
-  <div class="col-12">
-            
-    <div class="block" id="all">
-      <h1 class="block-title"><?php print drupal_get_title(); ?></h1>
+<div class="block" id="all">
+  <h1 class="block-title"><?php print drupal_get_title(); ?></h1>
       <?php if(!empty($academic_years)): ?>
       <form method="post" data-plugin="filters">
         <div class="row">
@@ -14,73 +11,64 @@
                 <span class="btn btn-xs btn-alternate"><?php print t('All'); ?></span>
               </span><!--/btn-input-->
               <?php foreach($academic_years as $delta => $academic_year): ?>
-              <span class="btn-input"<?php if($delta > 2) print ' data-show="hiddenFilter | inlineBlock"'; ?>>
+              <span class="btn-input">
                 <input type="checkbox" name="category" value="<?php print $academic_year->tid; ?>">
                 <span class="btn btn-xs btn-alternate"><?php print $academic_year->name; ?></span>
               </span><!--/btn-input-->
               <?php endforeach; ?>
-
-            </div><!--/btn-bar-->
-            <input type="hidden" name="page" id="paginatorNumber">
-            <?php if($bundle === 'article'): ?>
-            <input type="hidden" name="article_type" value="<?php if(current_path() === 'our-stories') {print 'our-stories';} else print 'news'; ?>">
-            <?php endif; ?>
-          </div><!--/col-9-->
-          
-          
-            <div class="col-3">
-            <?php if(count($academic_years) > 3): ?>
-               <a href="javascript:void(0);" class="cta-link pull-right before-plus" data-toggle="hiddenFilter" data-class="'active' : hiddenFilter"><?php print t('More choices'); ?></a>
-            <?php endif; ?>
-            </div><!--/col-3-->
-        </div><!--/row-->
-        
-        <div class="row-spacer-xs sm-hide"></div>
-        <hr />
-        <div class="row-spacer sm-hide"></div>
-          
-        <div class="row">
-          <div class="col-12">
-             <div class="form-item-row">
-                
-                <div class="form-item after-search form-item_rounded form-item-stretch">
-                   <div class="form-item_title"><?php print t('Search by title'); ?></div>
-                   <input type="text" name="title" placeholder="<?php print $search_placeholder; ?>" data-plugin="autocomplete" data-url="<?php print url("api/$bundle-autocomplete"); ?>" data-onSubmit="true" />
-                </div><!--/form-item-->
-                
-                <div class="form-item form-item_rounded after-calendar form-item-stretch">
-                   <div class="form-item_title"><?php print t('Date'); ?></div>
-                   <input type="text" name="date" data-plugin="datepickerRange" data-onSubmit="true" placeholder="<?php print t('Pick date (period)'); ?>" />
-                </div><!--/form-item-->
-                
-                <div class="row-spacer-xs sm-show"></div>
-                
-                <div class="form-item">
-                   <div class="form-item_title sm-hide">&nbsp;</div>
-                   <button class="btn btn-filled"><?php print t('Search'); ?></button>
-                </div><!--/form-item-->
-                
-             </div><!--/form-item-row-->
-          </div><!--/col-12-->
-        </div><!--/row-->
-        
-      <div class="row-spacer sm-hide"></div>
-      </form>
+        </div><!--/btn-bar-->
+        <input type="hidden" name="page" id="paginatorNumber">
+        <?php if($bundle === 'article'): ?>
+        <input type="hidden" name="article_type" value="<?php if(current_path() === 'our-stories') {print 'our_stories';} else print 'news'; ?>">
+        <?php endif; ?>
+      </div><!--/col-12-->
       
-      <div class="row-spacer sm-hide"></div>
-      <div class="row-spacer-xs sm-show"></div>
-      <hr class="sm-show">
-      <?php endif; ?>
-      <?php if($bundle === 'article'): ?>
-      <div data-posturl="<?php print url('api/articles'); ?>" data-plugin="filterContents">
-      <?php elseif($bundle === 'gallery'): ?>
-      <div data-posturl="<?php print url('api/gallery'); ?>" data-plugin="filterContents">
-      <?php else: ?>
-      <div>
-      <?php endif; ?>
-      <?php print $output; ?>
-      </div>
-    </div><!--/block-->
+    </div><!--/row-->
     
-  </div><!--/col-12-->
-</div><!--/row-->
+    <div class="row-spacer-xs sm-hide"></div>
+    <hr />
+    <div class="row-spacer sm-hide"></div>
+      
+    <div class="row">
+      <div class="col-12">
+         <div class="form-item-row">
+            
+            <div class="form-item after-search form-item_rounded form-item-stretch">
+               <div class="form-item_title"><?php print t('Search by title'); ?></div>
+               <input type="text" name="title" placeholder="<?php print $search_placeholder; ?>" data-plugin="autocomplete" 
+               data-url="<?php print url("api/$bundle-autocomplete" . ($bundle === 'article' ? "-$content_type" : "")); ?>" data-onSubmit="true" />
+            </div><!--/form-item-->
+            
+            <div class="form-item form-item_rounded after-calendar form-item-stretch">
+               <div class="form-item_title"><?php print t('Date'); ?></div>
+               <input type="text" name="date" data-plugin="datepickerRange" data-onSubmit="true" placeholder="<?php print t('Pick date (period)'); ?>" />
+            </div><!--/form-item-->
+            
+            <div class="row-spacer-xs sm-show"></div>
+            
+            <div class="form-item">
+               <div class="form-item_title sm-hide">&nbsp;</div>
+               <button class="btn btn-filled"><?php print t('Search'); ?></button>
+            </div><!--/form-item-->
+            
+         </div><!--/form-item-row-->
+      </div><!--/col-12-->
+    </div><!--/row-->
+    
+  <div class="row-spacer sm-hide"></div>
+  </form>
+  
+  <div class="row-spacer sm-hide"></div>
+  <div class="row-spacer-xs sm-show"></div>
+  <hr class="sm-show">
+  <?php endif; ?>
+  <?php if($bundle === 'article'): ?>
+  <div data-posturl="<?php print url('api/articles'); ?>" data-plugin="filterContents">
+  <?php elseif($bundle === 'gallery'): ?>
+  <div data-posturl="<?php print url('api/gallery'); ?>" data-plugin="filterContents">
+  <?php else: ?>
+  <div>
+  <?php endif; ?>
+  <?php print $output; ?>
+  </div>
+</div><!--/block-->
