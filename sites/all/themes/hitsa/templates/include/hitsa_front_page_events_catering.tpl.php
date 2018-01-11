80e9927961498db94998ac0bd68b9b04fcdd0ca1
Madis Loik <forzakenus@gmail.com>
2018-01-11 15:48:55 +0200

    Cateing sündmuste blokk esilehele

---
+++ sites/all/themes/hitsa/templates/include/hitsa_front_page_events_catering.tpl.php
@@ -0,0 +1,8 @@
+<div class="row">
+               <div class="col-6 sm-12">
+                   <?php print($events)?>
+               </div>
+               <div class="col-6 sm-12">
+                   <?php print($catering)?>
+               </div>
+</div>
