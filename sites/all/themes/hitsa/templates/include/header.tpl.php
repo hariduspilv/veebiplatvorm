72adddac5e11764084c5d111194f82ce4b7767e2
Madis Loik <forzakenus@gmail.com>
2018-01-29 09:47:18 +0200

    Header changes

--- sites/all/themes/hitsa/templates/include/header.tpl.php
+++ sites/all/themes/hitsa/templates/include/header.tpl.php
@@ -15,6 +15,7 @@
 
 <div class="inline">
    <div class="row">
+     <?php if(!isset($header_menu_1)):?>
       <div class="col-9">
         <?php if(!empty($site_logo)): ?>
         <a href="<?php print url('<front>'); ?>" class="header-logo"><img alt="" src="<?php print $site_logo; ?>" /></a>
@@ -23,6 +24,19 @@
       <div class="col-3">
         <?php print render($header_menu); ?>
       </div><!--/col-3-->
+        <?php else:?>
+          <div class="col-6">
+            <?php if(!empty($site_logo)): ?>
+            <a href="<?php print url('<front>'); ?>" class="header-logo"><img alt="" src="<?php print $site_logo; ?>" /></a>
+            <?php endif; ?>
+          </div><!--/col-9-->
+          <div class="col-3">
+            <?php print render($header_menu_0); ?>
+          </div><!--/col-3-->
+          <div class="col-3">
+            <?php print render($header_menu_1); ?>
+          </div><!--/col-3-->
+        <?php endif?>
    </div><!--/row-->
 </div><!--/inline-->
 
