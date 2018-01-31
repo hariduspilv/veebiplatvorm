9de67cb5f5af7f9f5b08ccbf2b95f34ffaf3e458
Madis Loik <madis@wiseman.ee>
2018-01-31 10:01:38 +0000

    Static regular folder

---
+++ sites/all/themes/hitsa/static/static2/Gruntfile.js
@@ -0,0 +1,172 @@
+module.exports = function (grunt) {
+	
+   grunt.loadNpmTasks('grunt-includes');
+   grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+   grunt.loadNpmTasks('grunt-contrib-less');
+   grunt.loadNpmTasks('grunt-contrib-connect');
+   grunt.loadNpmTasks('grunt-webfont');
+   grunt.loadNpmTasks('grunt-px-to-rem');
+   grunt.loadNpmTasks('grunt-autoprefixer');
+   grunt.loadNpmTasks('grunt-open');
+   
+   var port = 8080; //Math.floor(Math.random() * (9000 - 8000 + 1)) + 8000;
+   
+   grunt.initConfig({
+      
+      connect: {
+         server: {
+            options: {
+               port: port
+            }
+         }
+      },
+      
+      open : {
+         dev: {
+            path: 'http://127.0.0.1:'+port
+         }
+      },
+
+      autoprefixer: {
+         options:{
+           browsers: ['opera >= 11', 'ff >= 3', 'chrome >= 4', 'ie >= 9'] 
+         },
+         default_css: {
+            src: "assets/styles/default.css",
+            dest: "assets/styles/default.css"
+         },
+      },
+      
+      px_to_rem: {
+         dist: {
+            options: {
+               base: 16,
+               fallback: false,
+               fallback_existing_rem: false,
+               ignore: [],
+               map: false
+            },
+            files: {
+               'assets/styles/default.css': ['assets/styles/default.css']
+            }
+         }
+      },
+
+      webfont: {
+         icons: {
+            src: 'dev/glyphs/*.svg',
+            dest: 'assets/fonts/',
+            destLess: "dev/styles/",
+            options: {
+               stylesheets: ['less'],
+               template: "dev/glyphs/template.less",
+               relativeFontPath: "../fonts/",
+               fontFilename: 'iconFont',
+               types: "eot,woff2,woff,ttf,svg",
+               engine: "node",
+               normalize: true,
+               optimize: true
+            }
+         }
+      },
+      
+		concat: {
+         components: {
+            src: [
+              'dev/js/*.js'
+            ],
+            dest: 'assets/js/main.js'
+         },
+         components_dev: {
+            src: [
+               'dev/js/*.js',
+               '!dev/js/jquery-1.10.2.min.js'
+            ],
+            dest: 'assets/js/main-drupal.js'
+         }
+      },
+      
+		uglify: {
+         options: {
+            mangle: false
+         },
+         js: {
+            src: ['assets/js/main.js'],
+            dest: 'assets/js/main.js'
+         }
+      },
+      
+      less: {
+			options: {
+				compress: true
+			},
+			development: {
+				files: {
+					"assets/styles/default.css": "dev/styles/build.less"
+				}
+			}
+			
+		},
+      
+      includes: {
+         files: {
+            expand: "true",
+            src: ['*.html'],
+            dest: '',
+            cwd: 'dev/templates/',
+            options: {
+               silent: true
+            }
+         }
+      },
+	   
+      watch: {
+         html: {
+            files: ["dev/templates/**/*.html"],
+            tasks: ['includes']
+         },
+         css: {
+				files: ["dev/styles/*.less"],
+				tasks: ['less', 'px_to_rem', 'autoprefixer']
+			},
+			js: {
+            files: ['dev/js/*.js'],
+            tasks: ['concat', 'uglify'],
+            options: {
+               nospawn: true
+            }
+			},
+         iconfont: {
+				files: ["dev/glyphs/*.svg"],
+				tasks: ['webfont', 'less']
+			}
+      }
+      
+   });
+   
+   var tasks = {
+      webfont: true,
+      includes: true,
+      concat: true,
+      uglify: true,
+      less: true,
+      px_to_rem: true,
+      autoprefixer: true,
+      connect: true,
+      open: true,
+      watch: true
+   };
+   
+   var tasksArray = new Array();
+   
+   for( var i in tasks ){
+      if( tasks[i] ){
+         tasksArray.push(i);
+      }   
+   }
+   
+   grunt.registerTask('default', tasksArray);
+   
+};
---
+++ sites/all/themes/hitsa/static/static2/dev/js/map.js
@@ -0,0 +1,226 @@
+var mapStyles = [
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#e9e9e9"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 29
+            },
+            {
+                "weight": 0.2
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 18
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#dedede"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": 36
+            },
+            {
+                "color": "#333333"
+            },
+            {
+                "lightness": 40
+            }
+        ]
+    },
+    {
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            },
+            {
+                "lightness": 19
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 17
+            },
+            {
+                "weight": 1.2
+            }
+        ]
+    }
+];
+
+$.fn.googleMaps = function(){
+   var main = $(this);
+	var zoom = main.attr("data-zoom") || 17;
+	var mapObj, marker;
+
+   var icon = {
+      url: main.attr("data-icon"),
+      scaledSize: new google.maps.Size(28, 40)
+   };
+   
+   var latlng = {
+      lat: parseFloat( main.attr("data-coords").split(",")[0] ),
+      lng: parseFloat( main.attr("data-coords").split(",")[1] )
+   };
+	
+	function initialize(){
+		mapObj = new google.maps.Map(main[0], {
+			scrollwheel: false,
+			center: latlng,
+			zoom: parseInt(zoom),
+			mapTypeControl: false,
+			styles: mapStyles
+		});
+
+		marker = new google.maps.Marker({
+			position: latlng,
+			icon: icon
+		});
+
+		marker.setMap(mapObj);
+		
+	};
+   
+   
+	$(window).bind("resize:googleMaps", function(){
+		if( mapObj ){
+			mapObj.setCenter(latlng);
+		}
+	});
+	
+	main.bind("dynamic:resize", function(){
+		setTimeout(function(){
+			initialize();
+		}, 100);
+	});
+	
+	initialize();
+	
+};
