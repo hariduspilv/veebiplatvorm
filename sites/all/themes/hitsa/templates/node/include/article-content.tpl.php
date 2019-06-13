69d5a19820d4351cfa9adcdd9c8d5fd216dc3bc7
Madis Loik <forzakenus@gmail.com>
2019-06-13 09:12:52 +0300

    Catering template fix

--- sites/all/themes/hitsa/templates/node/include/article-content.tpl.php
+++ sites/all/themes/hitsa/templates/node/include/article-content.tpl.php
@@ -1,15 +1,15 @@
 
 <div class="block<?php if(!empty($hide_main_block)) print ' sm-hide'; ?>">
-  <?php if(!empty($article_type) && !empty($article_types_list[$article_type[0]['value']])): 
+  <?php if(!empty($article_type) && !empty($article_types_list[$article_type[0]['value']])):
   $heading_title = check_plain($article_types_list[$article_type[0]['value']]);
   ?>
   <h2 class="block-title"><?php print $heading_title; ?></h2>
-  <?php elseif($node->type === 'content_page' && $node->cp_type[LANGUAGE_NONE][0]['value'] === 'cp_service'): 
+  <?php elseif($node->type === 'content_page' && $node->cp_type[LANGUAGE_NONE][0]['value'] === 'cp_service'):
   $heading_title = t('Services');
   ?>
   <h2 class="block-title"><?php print $heading_title; ?></h2>
 
-  <?php else: 
+  <?php else:
     $active_menu_trail = menu_get_active_trail(); // Set active menu trail
     if(count($active_menu_trail) === 3) {
       $heading_title = t($active_menu_trail[1]['link_title']);
@@ -21,14 +21,14 @@
   <?php if(!empty($contact_filter)): ?>
     <?php print $contact_filter; ?>
   <?php endif; ?>
-  
+
   <div class="row-spacer-xs sm-hide"></div>
 
   <div class="row"<?php if(!empty($contact_filter)) {print ' data-posturl="' . url('api/contact') . '" data-plugin="filterContents"';} ?>>
 
     <?php if(!empty($content['cp_type']) && $content['cp_type']['#items'][0]['value'] === 'cp_contacts'): ?>
       <?php if(!empty($content['cp_contacts'])): ?>
-        <?php 
+        <?php
         $i = 0;
         while(!empty($content['cp_contacts']['#items'][$i])): ?>
         <div class="col-12">
@@ -42,12 +42,38 @@
     <?php else: ?>
     <div class="col-12">
       <article class="clearSpace">
-        
-      <div class="btn-bar align-right sm-show sm-pull_right">
+        <?php if(!empty($field_school_selections) && $field_school_selections[0]['value']=='catering'):?>
+                  <div class="article-heading">
+                  <h1><?php print $title?></h1>
+                  <?php if(!empty($content['field_catering_file'])):?>
+                    <?php foreach($field_catering_file as $catering_file):?>
+                        <a target="_blank" class="link-circle" href="<?php print $catering_file['url']?>">
+                            <span class="before-document"></span>
+                            <?php print $catering_file['name']?>
+                        </a>
+
+                      <?php endforeach?>
+                  <?php endif?>
+                </div><!--/article-heading-->
+                <div class="row-spacer"></div>
+        <?php endif;?>
+
+          <?php if(empty($field_school_selections)): ?>
+            <div class="btn-bar align-right sm-show sm-pull_right">
               <a href="javascript:void(0);" class="btn-circle before-share" data-plugin="share"></a>
               <a href="javascript:window.print();" class="sm-hide btn-circle before-print"></a>
             </div>
-        <h1 class="col-7 sm-12"><?php print $title; ?>
+          <?php elseif($field_school_selections[0]['value']!='catering'):?>
+            <div class="btn-bar align-right sm-show sm-pull_right">
+              <a href="javascript:void(0);" class="btn-circle before-share" data-plugin="share"></a>
+              <a href="javascript:window.print();" class="sm-hide btn-circle before-print"></a>
+            </div>
+            <?php endif?>
+          <?php if(empty($field_school_selections)): ?>
+            <h1 class="col-7 sm-12"><?php print $title; ?>
+          <?php elseif($field_school_selections[0]['value']!='catering'):?>
+            <h1 class="col-7 sm-12"><?php print $title; ?>
+          <?php endif?>
           <?php if($node->type === 'article'): ?>
 
           <span class="editor-info">
@@ -64,17 +90,10 @@
                     <div class="col-12 sm-12">
                     <?php else:?>
                     <div class="col-7 sm-12">
-                        <?php endif?>
-                    <?php if(!empty($content['field_catering_file'])):?>
-                    <?php foreach($field_catering_file as $catering_file):?>
-                        <a target="_blank" class="link-circle" href="<?php print $catering_file['url']?>">
-                            <span class="before-document"></span>
-                            <?php print $catering_file['name']?>
-                        </a>
-                            <div class="row-spacer"></div>
-                      <?php endforeach?>
-                    <?php endif?>
-        
+                <?php endif?>
+
+
+
             <?php if(!empty($body)): ?>
             <?php if(!empty($body[0]['safe_summary'])): ?>
             <div class="intro">
@@ -125,7 +144,7 @@
               </p>
             <?php endif; ?>
 
-            <?php if(!empty($cinfo_homepage_url) || !empty($cinfo_e_mail) 
+            <?php if(!empty($cinfo_homepage_url) || !empty($cinfo_e_mail)
             || !empty($cinfo_phone_nr) || !empty($cinfo_address)
             || !empty($cinfo_contact_person) || !empty($cinfo_contact_person_profession)
             ): ?>
@@ -193,28 +212,28 @@
             <span class="article-date sm-hide">
               <i><?php print t('Last changed') . ': ' . date('d.m.Y', $node->changed); ?></i>
             </span>
-            
+
 				  </div>
                       <?php    if($node->type === 'content_page' && $node->cp_type[LANGUAGE_NONE][0]['value'] === 'cp_simple_without_images'):?>
                           <?php else:?>
                             <div class="col-4 sm-12 col-offset-1 sm-offset-0">
                               <?php endif?>
 
-            
+
             <div class="btn-bar align-right sm-hide">
               <a href="javascript:void(0);" class="btn-circle before-share" data-plugin="share"></a>
               <a href="javascript:window.print();" class="sm-hide btn-circle before-print"></a>
             </div>
-            
+
             <div class="row-spacer-xl no-print sm-hide"></div>
             <?php if(!empty($subpage_images) || (!empty($cp_image) && $subpage_images = $cp_image)): $image = $subpage_images[0]; // Gallery ?>
               <figure>
                 <a href="<?php print image_style_url('hitsa_article_modal_view', $image['uri']); ?>"
                 data-download="<?php print file_create_url($image['uri']); ?>"
-                data-plugin="modal" data-modal="image-<?php print $image['fid']; ?>" 
+                data-plugin="modal" data-modal="image-<?php print $image['fid']; ?>"
                 data-heading="<?php print $heading_title; ?>" data-closebutton="<?php print t('Close'); ?>"
                 <?php (!empty($image['field_file_image_title_text']))? print ' data-title="' . check_plain($image['field_file_image_title_text'][LANGUAGE_NONE][0]['value']) . '"': print 'data-title=""' ?>>
-                  <img src="<?php print image_style_url('hitsa_core_thumbnail', $image['uri']); ?>" 
+                  <img src="<?php print image_style_url('hitsa_core_thumbnail', $image['uri']); ?>"
                   alt="<?php if(!empty($image['field_file_image_alt_text'])) print check_plain($image['field_file_image_alt_text'][LANGUAGE_NONE][0]['value']); ?>">
                 </a>
                 <figcaption><?php if(!empty($image['field_file_image_title_text'])) print check_plain($image['field_file_image_title_text'][LANGUAGE_NONE][0]['value']); ?></figcaption>
@@ -222,16 +241,16 @@
             <?php endif; ?>
             <?php if(!empty($article_video)): ?>
             <figure>
-              <a href="<?php print file_create_url($article_video[0]['uri']); ?>" 
-              data-title="<?php print check_plain($article_video[0]['filename']); ?>" 
+              <a href="<?php print file_create_url($article_video[0]['uri']); ?>"
+              data-title="<?php print check_plain($article_video[0]['filename']); ?>"
               data-plugin="modal" data-modal="video-<?php print $article_video[0]['fid']; ?>" data-closebutton="<?php print t('Close'); ?>"
               data-heading="<?php print check_plain($article_video[0]['filename']); ?>"
               <?php if(!empty($image['field_file_image_title_text'])) print ' data-title="' . check_plain($article_video[0]['filename']) . '"'; ?>>
                 <?php if(!empty($video_thumbnail)): ?>
-                <img src="<?php print $video_thumbnail; ?>" 
+                <img src="<?php print $video_thumbnail; ?>"
                 alt="<?php print check_plain($article_video[0]['filename']); ?>">
                 <?php else: ?>
-                <img src="<?php print image_style_url('hitsa_core_thumbnail', $image['uri']); ?>" 
+                <img src="<?php print image_style_url('hitsa_core_thumbnail', $image['uri']); ?>"
                 alt="<?php if(!empty($image['field_file_image_alt_text'])) print check_plain($image['field_file_image_alt_text'][LANGUAGE_NONE][0]['value']); ?>">
                 <?php endif; ?>
               </a>
@@ -239,32 +258,32 @@
             </figure>
             <?php elseif(!empty($subpage_images[1])): $image = $subpage_images[1]; ?>
             <figure>
-              <a href="<?php print image_style_url('hitsa_article_modal_view', $image['uri']); ?>" 
-              data-download="<?php print file_create_url($image['uri']); ?>" 
+              <a href="<?php print image_style_url('hitsa_article_modal_view', $image['uri']); ?>"
+              data-download="<?php print file_create_url($image['uri']); ?>"
               data-plugin="modal" data-modal="image-<?php print $image['fid']; ?>" data-heading="<?php print $heading_title; ?>" data-closebutton="<?php print t('Close'); ?>"
               <?php if(!empty($image['field_file_image_title_text'])) print ' data-title="' . check_plain($image['field_file_image_title_text'][LANGUAGE_NONE][0]['value']) . '"'; ?>>
-                <img src="<?php print image_style_url('hitsa_core_thumbnail', $image['uri']); ?>" 
+                <img src="<?php print image_style_url('hitsa_core_thumbnail', $image['uri']); ?>"
                 alt="<?php if(!empty($image['field_file_image_alt_text'])) print check_plain($image['field_file_image_alt_text'][LANGUAGE_NONE][0]['value']); ?>">
               </a>
               <figcaption><?php if(!empty($image['field_file_image_title_text'])) print check_plain($image['field_file_image_title_text'][LANGUAGE_NONE][0]['value']); ?></figcaption>
             </figure>
             <?php endif; ?>
-            
+
             <span class="article-date sm-show">
               <i><?php print t('Last changed') . ': ' . date('d.m.Y', $node->changed); ?></i>
             </span>
-            
+
           </div><!--/col-4-->
-          
+
 				</div>
-        
+
       </article>
-      
-      
+
+
     </div><!--/col-12-->
     <?php endif; ?>
   </div><!--/row-->
-  
+
 </div><!--/block-->
 <?php if(!empty($bottom_block)):?>
 <?php print $bottom_block?>
