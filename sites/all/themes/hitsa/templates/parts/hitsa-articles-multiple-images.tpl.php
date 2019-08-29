db2918b02ea4251b2646e774c17e8056464f62c3
Madis Loik <madis.loik@twn.ee>
2019-08-29 10:14:34 +0300

    Added article view hidden calendar in mobile

---
+++ sites/all/themes/hitsa/templates/parts/hitsa-articles-multiple-images.tpl.php
@@ -0,0 +1,23 @@
+<?php
+//krumo($images);
+
+?>
+<div class="row detail-pictures" data-plugin="detailPictures">
+  <?php if(!empty($images)):?>
+  <?php foreach($images as $key => $image):?>
+    <?php if($key ==0):?>
+      <div class="col-12">
+        <figure>
+          <a href="<?php print $image['full_url']?>" data-fancybox="fancybox" data-caption="<?php print $image['title']?>">
+            <img src="<?php print $image['thumbnail']?>" alt="<?php print $image['alt_text']?>">
+          </a>
+          <?php else:?>
+      <div class="col-6">
+        <figure class="little">
+          <a href="<?php print $image['full_url']?>" data-fancybox="fancybox" data-caption="<?php print $image['title']?>" style="background-image: url(<?php print $image['thumbnail']?>);"></a>
+    <?php endif?>
+        </figure>
+        </div>
+  <?php  endforeach;?>
+  <?php endif?>
+</div><!--/row-->
